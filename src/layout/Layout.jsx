e4ee9fdb2e44adea04530fd7a123e941a8d5cc01
Praptttiii <praptipatel12unjha@gmail.com>
2025-12-01 14:43:06 +0530

    created layout and added logout button in navbar

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/NavBar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "80px" }}>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
